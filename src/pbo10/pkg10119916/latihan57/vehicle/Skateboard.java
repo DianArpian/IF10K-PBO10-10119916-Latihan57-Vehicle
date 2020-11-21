6419ff740babadd7f5b965befb9003f3dbbd0a0f
DianArpian <>
2020-11-22 06:15:19 +0700

    Vehicle

---
+++ src/pbo10/pkg10119916/latihan57/vehicle/Skateboard.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10.pkg10119916.latihan57.vehicle;
+
+/**
+ *
+ * @author DIAN
+ */
+public class Skateboard extends Vehicle {
+     private double myBoardLenght;
+
+    public Skateboard(String myBrand, String myMOdel) {
+        super(myBrand, myMOdel);
+    }
+
+    public double getMyBoardLenght() {
+        return myBoardLenght;
+    }
+
+    public void setMyBoardLenght(double myBoardLenght) {
+        this.myBoardLenght = myBoardLenght;
+    }
+    
+}
+
